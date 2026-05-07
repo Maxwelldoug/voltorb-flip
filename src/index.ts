f6124e5be7af8f5af86e8a5c070e093cddc68b8f
Maxwell MacNeill <maxmacneill@hotmail.ca>
2026-05-07 15:54:46 -0300

    Use 'levelIndex' intermediate to calculate level stats, fixing off by 1

--- src/index.ts
+++ src/index.ts
@@ -711,7 +711,8 @@ updateSize()
 function setup() {
 	const tileValues = [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ]
 	const toSet = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24 ]
-	const levelStats = levels[level][Math.floor(Math.random() * 5)]
+	const levelIndex = Math.min(Math.max(level, 1), levels.length) - 1
+	const levelStats = levels[levelIndex][Math.floor(Math.random() * 5)]
 
 	copyMemoMode = false
 	sMemoButton.src = srcButtonMemoSOff
